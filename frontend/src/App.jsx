4fcf89a5a024d89c8f716ca8e24180253885391e
Abdullah <abdullahdeveloper843@gmail.com>
2025-11-04 11:39:56 +0500

    Some files from the root were removed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import FarmerDashboard from "./components/farmer/FarmerDashboard";
 import Navbar from "./components/global/Navbar";
 import AdminDashboard from "./components/admin/AdminDashboard";
+import Auth from "./pages/Auth";
 function App() {
   return (
     <>
       <Navbar />
-      <FarmerDashboard />
-      <AdminDashboard />
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/farmer" element={<FarmerDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
     </>
   );
 }
