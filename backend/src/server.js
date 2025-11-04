bea746764826dff44f990b3f963298a1d16b7a91
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2025-11-04 10:12:22 +0500

    refactor resp helper

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,17 +2,25 @@ const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
 
-const resp = require('./helpers/resp.helper');
+const { resp } = require('./helpers');
 const authRoutes = require("./routes/Auth.routes");
 const errorHandler = require("./middleware/ErrorHandler.middleware");
+
+// -------------------------------------------------------------------------- //
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("MongoDB connection error:", err));
+  .then(() => console.log("[INFO] MongoDB connected"))
+  .catch(error => console.error("[ERROR] MongoDB connection error:", error));
+
+// -------------------------------------------------------------------------- //
+
+app.get('/', (req, res) => {
+  resp(res, 200, "OK");
+})
 
 app.use("/api/auth", authRoutes);
 
@@ -21,6 +29,8 @@ app.use((req, res) => {
 })
 
 app.use(errorHandler);
+
+// -------------------------------------------------------------------------- //
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`[INFO] Server listening on port ${process.env.PORT || 3000}`)
